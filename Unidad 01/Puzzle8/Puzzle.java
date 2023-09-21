9e4c00d6a2d7f3a27c4608666aa4cf0dad6cd332
AnguloLopezJose <80418064+AnguloLopezJose@users.noreply.github.com>
2023-09-21 12:35:36 -0700

    Add files via upload

---
+++ Unidad 01/Puzzle8/Puzzle.java
@@ -0,0 +1,11 @@
+package puzzle8;
+
+public class Puzzle {
+    public static void main(String[] args) {
+       Grafico abrir=new Grafico();
+       abrir.setVisible(true);
+       abrir.setLocationRelativeTo(null);
+       abrir.setResizable(false);
+    }
+    
+}
